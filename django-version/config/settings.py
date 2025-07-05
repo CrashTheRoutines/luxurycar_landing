5a69716d51dc0b3150ff34c2f85c107003669b1f
Adam Lean <adam.lean.92@gmail.com>
2025-07-05 15:27:07 +0800

    .

--- django-version/config/settings.py
+++ django-version/config/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / '../../../luxurycar_landing' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / '../../styles/styles.css',
+    BASE_DIR / '../../assets/img',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
